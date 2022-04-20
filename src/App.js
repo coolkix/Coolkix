2e19f881b3b8917fba3ce86fa34a4e1110b6c9ef
coolkix <79751502+coolkix@users.noreply.github.com>
2022-04-20 17:08:36 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Veikali from "./components/Veikali"
-import Razotaji from "./components/Razotaji"
-import Produkti from "./components/Produkti"
-import Home from './components/Home'
-
-
-  
-
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Veikali from "./components/Veikali";
+import Razotaji from "./components/Razotaji";
+import Produkti from "./components/Produkti";
+import Home from "./components/Home";
 
 function App() {
-  
-
   return (
     <Router>
-      <div style={{background: '#95AD6F'}}><Header /></div>
-        
-          <Switch>
-          <Route path="/Home" component={ Home } />
-                <Route path="/Veikali" component={ Veikali } />
-                <Route path="/Razotaji" component={ Razotaji } />
-                <Route path="/Produkti" component={ Produkti } />
-          </Switch>
-          <Footer/>
+      <div style={{ background: "#95AD6F" }}>
+        <Header />
+      </div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Home" component={Home} />
+        <Route path="/Veikali" component={Veikali} />
+        <Route path="/Razotaji" component={Razotaji} />
+        <Route path="/Produkti" component={Produkti} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
